7ddd4f73e18d281a6f639911c5078c90099136c5
Jake Reeder <jacobdreeder@gmail.com>
2024-04-24 09:16:10 -0400

    Edited sql script and added cute icon

--- DatabaseQueries/Creation.sql
+++ DatabaseQueries/Creation.sql
@@ -3,22 +3,27 @@
 -- CREATE DATABASE Animal_Shelter;
 
 -- @block
+CREATE DATABASE Animal_Shelter;
+
+-- @block
+DROP DATABASE Animal_Shelter;
+
+-- @block
 BACKUP DATABASE Animal_Shelter
 TO DISK = "C:\Users\Chris\OneDrive\Documents\DB Backups";
 
 -- @block 
 -- Creating all of the needed tables for the data base.
+USE Animal_Shelter;
 
 CREATE TABLE Employees (
-    ID int NOT NULL AUTO_INCREMENT, 
-    FirstName varchar(50),
+    ID CHAR(6) NOT NULL UNIQUE, 
+    FirstName varchar(50) NOT NULL,
     LastName varchar(50) NOT NULL,
     Password varchar(50) NOT NULL, 
     Position ENUM('Director', 'Assistant Director', 'Employee') NOT NULL,
     PRIMARY KEY (ID)
 );
-
--- @block 
 CREATE TABLE Addresses (
     AddressID int NOT NULL AUTO_INCREMENT,
     StreetAddress varchar(255),
@@ -27,50 +32,55 @@ CREATE TABLE Addresses (
     PostalCode varchar(10),
     PRIMARY KEY (AddressID)
 );
-
--- @block 
 CREATE TABLE Customers (
     ID int NOT NULL AUTO_INCREMENT,
     FirstName varchar(50) NOT NULL,
     LastName varchar(50) NOT NULL,
-    PhoneNumber varchar(10) NOT NULL,
+    PhoneNumber varchar(12) NOT NULL,
     EmailAddress varchar(100) NOT NULL,
     StreetAddress int NOT NULL,
     PRIMARY KEY (ID),
     FOREIGN KEY (StreetAddress) REFERENCES Addresses(AddressID)
 );
-
--- @block 
 CREATE TABLE Animals (
     IntakeNumber int NOT NULL AUTO_INCREMENT,
     AnimalType ENUM('Small Animal', 'Livestock') NOT NULL,
-    Name varchar(50) NOT NULL,
+    Name varchar(50) NOT NULL UNIQUE,
     Sex ENUM('Male', 'Female') NOT NULL,
     SpayNeuterStatus boolean NOT NULL,
     VaccinationStatus boolean NOT NULL,
-    Age int NOT NULL,
-    ReasonForStay varchar(255),
-    AvailabilityDate date,
     Breed varchar(100),
-    Characteristics varchar(255),
-    SpecialNeeds varchar(255),
     EuthanasiaStatus boolean NOT NULL,
     CageNumber int NOT NULL,
     VetNumber varchar(10),
-    AuctionStatus boolean NOT NULL,
     PRIMARY KEY (IntakeNumber)
 );
-
--- @block 
 CREATE TABLE Transactions (
     ID int NOT NULL AUTO_INCREMENT,
-    Employee int NOT NULL,
+    Employee CHAR(6) NOT NULL,
     Animal int NOT NULL,
     Customer int NOT NULL,
     TransactionType ENUM('Adoption', 'Reclamation') NOT NULL,
-    Amount float(10) NOT NULL,
+    Amount DECIMAL(4,2) NOT NULL,
     PRIMARY KEY (ID),
     FOREIGN KEY (Employee) REFERENCES Employees(ID),
     FOREIGN KEY (Animal) REFERENCES Animals(IntakeNumber),
     FOREIGN KEY (Customer) REFERENCES Customers(ID)
 );
+
+-- @block
+INSERT INTO employees
+VALUES('W1TBRD', 'Ed', 'Newgate', '1LoveDog$', 'Assistant Director');
+
+ -- @block
+INSERT INTO Addresses(StreetAddress, City, State, PostalCode)
+VALUES ('1234 Sesame Street', 'Florence', 'SC', '29505')
+
+ -- @block
+INSERT INTO CustomerS(FirstName, LastName, PhoneNumber, EmailAddress, StreetAddress)
+VALUES ('TonyTony', 'Chopper', '800-867-5309', 'drchopper@gmail.com', 1)
+
+ -- @block
+INSERT INTO Animals (AnimalType, Name, Sex, SpayNeuterStatus, VaccinationStatus, 
+ Breed, EuthanasiaStatus, CageNumber, VetNumber)
+ VALUES ('Small Animal', 'Biscuit', 'Female', TRUE, TRUE, 'Corgi', FALSE, 3, NULL)
